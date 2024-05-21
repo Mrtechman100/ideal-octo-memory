b59d7beb85e2acbcc65e5c26e2dc21464bdaa67f
Dallin M. Clarke <mrtechman100@gmail.com>
2024-05-21 10:10:03 -0600

    Changed some functionality code
    I made it so that the image button looked like a button. I made it so that when the modal is open, the contents in the background don't scroll.

--- TemplateIdeas/HomePageIdeas/MobileFirst_Homepage/modalScripts.js
+++ TemplateIdeas/HomePageIdeas/MobileFirst_Homepage/modalScripts.js
@@ -4,8 +4,10 @@ var span = document.getElementsByClassName('close')[0];
 
 modalBtn.onclick = function() {
   modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 }
 
 span.onclick = function() {
   modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
 }
